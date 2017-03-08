165df68ec1362522eef9013538ba63f5e0e45fcb
Aneeque Safdar <Aneeque Safdar>
2017-03-08 06:02:23 +0000

    get block with transaction objects from the blockchain given block number/hash

--- server/controllers/block.js
+++ server/controllers/block.js
@@ -1,8 +1,18 @@
 const express  = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.send('GET block');
+router.get('/:id', function(req, res) {
+  var blockNumberOrHash = req.params.id;
+  var block = web3.eth.getBlock(blockNumberOrHash);
+  console.log(block);
+  res.json(block);
 });
+
+router.get('/:id/transactions', function(req, res) {
+  var blockNumberOrHash = req.params.id;
+  var block = web3.eth.getBlock(blockNumberOrHash, true);
+  console.log(block);
+  res.json(block);
+})
 
 module.exports = router;
